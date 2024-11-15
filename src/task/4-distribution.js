3c281c4579ab9c71d54e9eee7dfb9836a4495b4c
zznam <namnam55@proton.me>
2024-11-15 13:59:26 +0700

    feat: add validation for submitters

--- src/task/4-distribution.js
+++ src/task/4-distribution.js
@@ -1,6 +1,6 @@
 const SLASH_PERCENT = 0.7;
 
-export function distribution(submitters, bounty, roundNumber) {
+export async function distribution(submitters, bounty, roundNumber) {
   /**
    * Generate the reward list for a given round
    * This function should return an object with the public keys of the submitters as keys
@@ -9,6 +9,8 @@ export function distribution(submitters, bounty, roundNumber) {
   console.log(`MAKE DISTRIBUTION LIST FOR ROUND ${roundNumber}`);
   const distributionList = {};
   const approvedSubmitters = [];
+  const startOfDayUTC = new Date(Date.UTC(new Date().getUTCFullYear(), new Date().getUTCMonth(), new Date().getUTCDate()));
+
   // Slash the stake of submitters who submitted incorrect values
   // and make a list of submitters who submitted correct values
   for (const submitter of submitters) {
@@ -19,6 +21,14 @@ export function distribution(submitters, bounty, roundNumber) {
       distributionList[submitter.publicKey] = -slashedStake;
       console.log('CANDIDATE STAKE SLASHED', submitter.publicKey, slashedStake);
     } else {
+      // check submitter is valid for distribution
+      const userData = await validateSubmitterForDistribution(submitter);
+      if (!userData?.distributable) {
+        console.log('CANDIDATE NOT VALID FOR DISTRIBUTION', submitter.publicKey);
+        continue
+      } else {
+        await updateLastDistributionAt(userData, startOfDayUTC);
+      }
       approvedSubmitters.push(submitter.publicKey);
     }
   }
@@ -29,4 +39,55 @@ export function distribution(submitters, bounty, roundNumber) {
     distributionList[candidate] = reward;
   });
   return distributionList;
+}
+
+const USER_DISTRIBUTION_ENDPOINT = 'http://localhost:5001/api';
+const X_API_KEY = 'supersecret';
+
+async function validateSubmitterForDistribution(submitter) {
+  try {
+    const response = await axios.post(
+      `${USER_DISTRIBUTION_ENDPOINT}/validate-distribution`,
+      {
+        koii_main_account_pubkey: submitter.publicKey,
+        email: submitter.email
+      },
+      {
+        headers: {
+          'x-api-key': X_API_KEY,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    console.log("VALIDATE DISTRIBUTION RESPONSE DATA:", response.data?.data);
+    return response.data?.data;
+  } catch (error) {
+    console.error('Error validating submitter for distribution:', error);
+    return null
+  }
+}
+
+
+async function updateLastDistributionAt(user, newDistributionDate) {
+  try {
+    const response = await axios.put(
+      `${USER_DISTRIBUTION_ENDPOINT}/user/${user.id}`,
+      {
+        koii_main_account_pubkey: user.koii_main_account_pubkey,
+        email: user.email,
+        last_distribution_at: newDistributionDate
+      },
+      {
+        headers: {
+          'x-api-key': X_API_KEY,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    console.log("UPDATE LAST DISTRIBUTION", response.data?.data);
+    return response.data?.data;
+  } catch (error) {
+    console.error('Error updating last distribution at:', error);
+    return null
+  }
 }
